ce579b6fde9263c4aa9afeb3fa1f14a07152b055
Jerish <jerish1998@gmail.com>
2025-05-21 19:17:12 +0530

    edited the sign up page

--- LibraryManaFRE/src/views/auth/RegisterView.vue
+++ LibraryManaFRE/src/views/auth/RegisterView.vue
@@ -1,64 +1,29 @@
 <template>
   <form >
-    
-    <div class="flex flex-col mx-auto md:w-96 w-full">
-      <h1 class="text-2xl font-bold mb-4 text-center">Register</h1>
-      <div class="flex flex-col gap-2 mb-4">
-        <label for="name" class="required">Name</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          class="form-input"
-          autocomplete="name"
-          required
-        />
-      </div>
- 
-      <div class="flex flex-col gap-2 mb-4">
-        <label for="email" class="required">Email</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          class="form-input"
-          autocomplete="email"
-          required
-        />
-      </div>
- 
-      <div class="flex flex-col gap-2 mb-4">
-        <label for="password" class="required">Password</label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          class="form-input"
-          autocomplete="new-password"
-          required
-        />
-      </div>
- 
-      <div class="flex flex-col gap-2">
-        <label for="password_confirmation" class="required">
-          Confirm password
-        </label>
-        <input
-          id="password_confirmation"
-          name="password_confirmation"
-          type="password"
-          class="form-input"
-          autocomplete="new-password"
-          required
-        />
-      </div>
- 
-      <div class="border-t h-[1px] my-6"></div>
- 
-      <div class="flex flex-col gap-2">
-        <button type="submit" class="btn btn-primary" @click="handleClick">Register</button>
-      </div>
+    <div>
+<div>
+    <h1>REGISTER</h1>
+</div>
+<div>
+    <form @submit.prevent="handleSubmit">
+    <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name">
     </div>
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email">
+    </div>
+    <div class="form-group">
+        <label for="name">Password:</label>
+        <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">Sign Up</button>
+    </form>
+</div>
+
+    </div>
+    
   </form>
 </template>
 
@@ -66,7 +31,7 @@
 export default{
 methods:{
 
-handleClick(){
+async handleSubmit(){
     console.log("Handele click");
 }
 
