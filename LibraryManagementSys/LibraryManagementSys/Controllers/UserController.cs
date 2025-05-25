31caa1535640f4e636fd0643c52ca5350cdc88ab
Jerish s mathew <128453069+jerish998@users.noreply.github.com>
2025-05-25 21:29:10 +0530

    Dto models add login (#5)
    * added interfaces, service implemented for user , added controller
    
    * added models , dto by seperate class libraary
    
    * added login page models
    
    * added main .css file
    
    * addede files for styles
    
    * edited the code added packages
    
    * edited the sign up page
    
    * added files provider for connection
    
    * removed files vue
    
    * edited file code
    
    * addded middle ware for run
    
    * Added middle Ware https

--- LibraryManagementSys/LibraryManagementSys/Controllers/UserController.cs
+++ LibraryManagementSys/LibraryManagementSys/Controllers/UserController.cs
@@ -15,47 +15,57 @@ namespace LibraryManagementSys.Controllers
         public UserController(UserService userService)
         {
             _userService = userService;
+
         }
 
         #endregion
-        [HttpPost]
-        public async Task<IActionResult>  AddUser(User user_details)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+      
 
-            if (!user_details.ContactNumber.Any())
-        {
-                return BadRequest("Requires Data missing");
-        }
-        UserDto user_dto = new UserDto();
-            user_dto.Name = user_details.UserName;
-            user_dto.Password = user_details.Password;
-            user_dto.Email = user_details.Email;
-              await _userService.CreateUser(user_dto);
+        //[HttpPost]
+        //public async Task<IActionResult>  AddUser(User user_details)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return BadRequest(ModelState);
+
+   //         return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+
+   //     }
 
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+        //    if (!user_details.ContactNumber.Any())
+        //{
+        //        return BadRequest("Requires Data missing");
+        //}
+        //UserDto user_dto = new UserDto();
+        //    user_dto.Name = user_details.UserName;
+        //    user_dto.Password = user_details.Password;
+        //    user_dto.Email = user_details.Email;
+        //      await _userService.CreateUser(user_dto);
 
-            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
 
-        }
+        //    //_context.Users.Add(user);
+        //    //await _context.SaveChangesAsync();
+
+        //    //return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+
+        //}
 
 
-        [HttpPost]
-        public async Task<IActionResult> CreateUser([FromBody] UserCreateDto dto)
-        {
+        //[HttpPost]
+        //public async Task<IActionResult> CreateUser([FromBody] UserCreateDto dto)
+        //{
            
 
-            var user = new User
-            {
-                FullName = dto.FullName,
-                Email = dto.Email
-            };
+        //    var user = new User
+        //    {
+        //        FullName = dto.FullName,
+        //        Email = dto.Email
+        //    };
 
            
-        }
+        //}
+
+    
 
 
 
--- LibraryManagementSys/LibraryManagementSys/Program.cs
+++ LibraryManagementSys/LibraryManagementSys/Program.cs
@@ -1,25 +1,50 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using LibraryManagementSys.Extensions;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+
+
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.WebHost.ConfigureKestrel(serverOptions => {
+            serverOptions.ListenAnyIP(5000);//HTTP
+            serverOptions.ListenAnyIP(5001,listenOptions => {
+                listenOptions.UseHttps();//HTTPS
+            });
+        });
+
+
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Register custom middleware early in the pipeline
+        app.UseRequestLogging(); //Your custom middleware
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
