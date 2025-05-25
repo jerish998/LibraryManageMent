8913f0946f19ef65b1265f4b4374a3438bbaab09
Jerish <jerish1998@gmail.com>
2025-05-25 21:07:57 +0530

    addded middle ware for run

---
+++ LibraryManagementSys/LibraryManagementSys/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementSys.Middlewares;
+
+namespace LibraryManagementSys.Extensions
+{
+    // Extensions/MiddlewareExtensions.cs
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggingMiddleware>();
+        }
+    }
+
+}
---
+++ LibraryManagementSys/LibraryManagementSys/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace LibraryManagementSys.Middlewares
+{
+    // Middlewares/RequestLoggingMiddleware.cs
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var watch = Stopwatch.StartNew();
+
+            _logger.LogInformation("Handling request: {method} {url}",
+                context.Request.Method,
+                context.Request.Path);
+
+            await _next(context);
+
+            watch.Stop();
+            _logger.LogInformation("Finished handling request. Took {time} ms", watch.ElapsedMilliseconds);
+        }
+    }
+
+}
