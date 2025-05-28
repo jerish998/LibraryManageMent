44d06ed38be9e48c1407739b779c1f96bde82c6f
Jerish <jerish1998@gmail.com>
2025-05-28 22:48:34 +0530

    added sample jwt tokens generat the towken

--- LibraryManagementSys/Models/User.cs
+++ LibraryManagementSys/Models/User.cs
@@ -8,4 +8,10 @@
         public string Email { get; set; }
 
     }
+
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
